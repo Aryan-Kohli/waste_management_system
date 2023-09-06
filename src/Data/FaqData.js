fee05b5dab428b7f2ede17227ced06c6e22f39f6
Aryan-Kohli <106698463+Aryan-Kohli@users.noreply.github.com>
2023-09-06 23:22:01 +0530

    Added Faq section
    Completed added faq section

---
+++ src/Data/FaqData.js
@@ -0,0 +1,53 @@
+export default [
+    {
+        id:1,
+        Question:"What is waste management?",
+        Answer:"Waste management refers to the collection, transportation, processing, recycling, and disposal of waste materials, with the goal of minimizing environmental impact and promoting public health and safety.",
+    }   ,
+    {
+        id:2,
+        Question:"Why is proper waste management important?",
+        Answer:"Proper waste management is crucial for several reasons. It helps prevent environmental pollution, conserves resources, reduces greenhouse gas emissions, and minimizes health hazards associated with waste. It also contributes to a sustainable and cleaner environment.",
+    }  , 
+    {
+        id:3,
+        Question:"What are the primary methods of waste disposal?",
+        Answer:"The primary methods of waste disposal include landfilling (burying waste in designated areas), recycling (reusing materials from waste), incineration (burning waste to generate energy), composting (turning organic waste into nutrient-rich compost), and hazardous waste disposal (specialized methods for toxic materials).",
+    }   ,
+    {
+        id:4,
+        Question:"How can I reduce my household waste?",
+        Answer:"  You can reduce household waste by practicing the 3R's Reduce (use less and generate less waste), Reuse (repurpose items when possible), and Recycle (participate in local recycling programs). Additionally, composting organic waste and buying products with minimal packaging can help reduce waste "
+    },
+    {
+        id:5,
+        Question:"What should I do with hazardous waste, such as old paint or batteries?",
+        Answer:"Hazardous waste, like paint, batteries, and chemicals, should not be disposed of in regular trash. Instead, take them to designated hazardous waste collection centers or participate in local hazardous waste disposal programs to ensure safe and proper disposal."
+    },
+    {
+        id:6,
+        Question:"Can I recycle electronic waste (e-waste)?",
+        Answer:"Yes, electronic waste (e-waste) should be recycled separately from regular household waste. Many communities have e-waste recycling programs or collection events for items like computers, cell phones, and appliances."
+    },
+    {
+        id:7,
+        Question:"What can I do about large items, such as old furniture and appliances?",
+        Answer:" Large items like furniture and appliances can often be recycled or disposed of through special collection programs provided by your local waste management authority. You can also consider donating these items if they are still in good condition."
+    },
+    {
+        id:8,
+        Question:"Is it necessary to separate recyclables before disposal?",
+        Answer:"In many areas, recyclables should be separated from non-recyclables before disposal. This helps prevent contamination of recyclable materials and ensures they can be processed effectively. Follow your local recycling guidelines for specific instructions."
+    },
+    {
+        id:9,
+        Question:" What happens to my recyclables after they are collected?",
+        Answer:"After collection, recyclables are typically sorted, cleaned, and processed at recycling facilities. They are then turned into raw materials for manufacturing new products. The specific process may vary depending on the material and recycling facility."
+    },
+    {
+        id:10,
+        Question:"How can I find out more about waste management in my area?",
+        Answer:"To learn more about waste management in your area, contact your local waste management authority or visit their website. They can provide information on recycling programs, disposal guidelines, and collection schedules specific to your community."
+    },
+
+];
--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,9 +1,224 @@
 import React from "react";
+import Accordion from "react-bootstrap/Accordion";
 import "./Faq.css";
+import data from "../Data/FaqData";
 export default function Faq() {
   return (
-    <div>
-      <h1 className="mainhead text-center">FAQ will come here</h1>
+    <div className="container faqmain">
+      <h1 className="mainhead text-center">FAQs</h1>
+
+      <div className="accordion accordion-flush" id="accordionFlushExample">
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#flush-collapseOne"
+              aria-expanded="false"
+              aria-controls="flush-collapseOne"
+            >
+              {data[0].Question}
+            </button>
+          </h2>
+          <div
+            id="flush-collapseOne"
+            className="accordion-collapse collapse"
+            data-bs-parent="#accordionFlushExample"
+          >
+            <div className="accordion-body">{data[0].Answer}</div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#flush-collapseTwo"
+              aria-expanded="false"
+              aria-controls="flush-collapseTwo"
+            >
+              {data[1].Question}
+            </button>
+          </h2>
+          <div
+            id="flush-collapseTwo"
+            className="accordion-collapse collapse"
+            data-bs-parent="#accordionFlushExample"
+          >
+            <div className="accordion-body">{data[1].Answer}</div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#flush-collapseThree"
+              aria-expanded="false"
+              aria-controls="flush-collapseThree"
+            >
+              {data[2].Question}
+            </button>
+          </h2>
+          <div
+            id="flush-collapseThree"
+            className="accordion-collapse collapse"
+            data-bs-parent="#accordionFlushExample"
+          >
+            <div className="accordion-body">{data[2].Answer}</div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#flush-collapseFour"
+              aria-expanded="false"
+              aria-controls="flush-collapseFour"
+            >
+              {data[3].Question}
+            </button>
+          </h2>
+          <div
+            id="flush-collapseFour"
+            className="accordion-collapse collapse"
+            data-bs-parent="#accordionFlushExample"
+          >
+            <div className="accordion-body">{data[3].Answer}</div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#flush-collapseFive"
+              aria-expanded="false"
+              aria-controls="flush-collapseFive"
+            >
+              {data[4].Question}
+            </button>
+          </h2>
+          <div
+            id="flush-collapseFive"
+            className="accordion-collapse collapse"
+            data-bs-parent="#accordionFlushExample"
+          >
+            <div className="accordion-body">{data[4].Answer}</div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#flush-collapseSix"
+              aria-expanded="false"
+              aria-controls="flush-collapseSix"
+            >
+              {data[5].Question}
+            </button>
+          </h2>
+          <div
+            id="flush-collapseSix"
+            className="accordion-collapse collapse"
+            data-bs-parent="#accordionFlushExample"
+          >
+            <div className="accordion-body">{data[5].Answer}</div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#flush-collapseSeven"
+              aria-expanded="false"
+              aria-controls="flush-collapseSeven"
+            >
+              {data[6].Question}
+            </button>
+          </h2>
+          <div
+            id="flush-collapseSeven"
+            className="accordion-collapse collapse"
+            data-bs-parent="#accordionFlushExample"
+          >
+            <div className="accordion-body">{data[6].Answer}</div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#flush-collapseEight"
+              aria-expanded="false"
+              aria-controls="flush-collapseEight"
+            >
+              {data[7].Question}
+            </button>
+          </h2>
+          <div
+            id="flush-collapseEight"
+            className="accordion-collapse collapse"
+            data-bs-parent="#accordionFlushExample"
+          >
+            <div className="accordion-body">{data[7].Answer}</div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#flush-collapseNine"
+              aria-expanded="false"
+              aria-controls="flush-collapseNine"
+            >
+              {data[8].Question}
+            </button>
+          </h2>
+          <div
+            id="flush-collapseNine"
+            className="accordion-collapse collapse"
+            data-bs-parent="#accordionFlushExample"
+          >
+            <div className="accordion-body">{data[8].Answer}</div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#flush-collapseTen"
+              aria-expanded="false"
+              aria-controls="flush-collapseTen"
+            >
+              {data[9].Question}
+            </button>
+          </h2>
+          <div
+            id="flush-collapseTen"
+            className="accordion-collapse collapse"
+            data-bs-parent="#accordionFlushExample"
+          >
+            <div className="accordion-body">{data[9].Answer}</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
