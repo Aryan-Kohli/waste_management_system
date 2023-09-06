40a7173dc6b3e8928f781f17e5255f64ed511f0e
Aayush <aayushsharma47163@gmail.com>
2023-09-07 00:11:07 +0530

    Navbar and Hero section created

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,9 @@
-import React from "react";
 import "./Hero.css";
+
 export default function Hero() {
   return (
-    <div>
-      <h1>hero will be here</h1>
+    <div id = "hero-section">
+      <h1 id = "hero">An all in one<br /> waste management system <br /> that lets you keep your area <br /> CLEAN.</h1>
     </div>
   );
 }
