39e7f76dcfe950952c24e5e7c848a4009d3e1744
Aryan-Kohli <106698463+Aryan-Kohli@users.noreply.github.com>
2023-09-07 21:50:38 +0530

    changed logo font

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,5 @@ import React from "react";
 import "./Footer.css";
 
 export default function Footer() {
-  return (
-    <div>
-      <h1>footer will be here</h1>
-    </div>
-  );
+  return <div>{/* <h1>footer will be here</h1> */}</div>;
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,15 +2,21 @@ import "./Header.css";
 
 export default function header() {
   return (
-    <div id = "nav">
-      <h1 id = "title">GarbageGuardians</h1>
-        <a className = "link">About us</a>
-        <a className = "link">Recycling Centres</a>
-        <a className = "link">Complaint</a>
-        <a className = "link">Discussions</a>
-        <a className = "link">Contact</a>
-      <div id = "left-section">
-        <a id = "Login">Login | Signup</a>
+    <div id="nav">
+      <h1 id="title">Garbage Guardians</h1>
+      <a className="link" href="#AboutUs">
+        About us
+      </a>
+      <a className="link">Recycling Centres</a>
+      <a className="link" href="#Complaint">
+        Complaint
+      </a>
+      <a className="link">Discussions</a>
+      <a className="link">Contact</a>
+      <div id="left-section">
+        <div className="btn-success" id="Login">
+          Login | Signup
+        </div>
       </div>
     </div>
   );
