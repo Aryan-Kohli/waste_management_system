cb696e2465834c7e07792dc2ab5db804a9bffc8b
Aayush Sharma <aayushsharma47163@gmail.com>
2023-09-10 00:54:02 +0530

    Update Aboutus.jsx

--- src/components/Aboutus.jsx
+++ src/components/Aboutus.jsx
@@ -12,7 +12,7 @@ export default function Aboutus() {
         </div>
       <div id = "Cards-stack">
         <Card style={{ width: '24rem' }}>
-          <Card.Img variant="top" src="src\assets\quiz.svg" />
+          <Card.Img variant="top" src="../src/assets/quiz.svg" />
           <Card.Body>
             <Card.Title>Interactive Quizzes</Card.Title>
             <Card.Text>
@@ -22,7 +22,7 @@ export default function Aboutus() {
           </Card.Body>
        </Card>
        <Card style={{ width: '24rem' }}>
-          <Card.Img variant="top" src="src\assets\ngo.jpeg" />
+          <Card.Img variant="top" src="../src/assets/ngo.jpeg" />
           <Card.Body>
             <Card.Title>Collaboration with Non Goverment Organizations.</Card.Title>
             <Card.Text>
@@ -32,7 +32,7 @@ export default function Aboutus() {
           </Card.Body>
         </Card>
         <Card style={{ width: '24rem' }}>
-          <Card.Img variant="top" src="src\assets\modules.jpg" />
+          <Card.Img variant="top" src="../src/assets/modules.jpg" />
           <Card.Body>
             <Card.Title>Informative Educational Modules and Resources</Card.Title>
             <Card.Text>
@@ -42,7 +42,7 @@ export default function Aboutus() {
           </Card.Body>
         </Card>
         <Card style={{ width: '24rem' }}>
-          <Card.Img variant="top" src="src\assets\reward.jpg" />
+          <Card.Img variant="top" src="../src/assets/reward.jpg" />
           <Card.Body>
             <Card.Title>Society of the month Competitions.</Card.Title>
             <Card.Text>
@@ -52,7 +52,7 @@ export default function Aboutus() {
           </Card.Body>
         </Card>
         <Card style={{ width: '24rem' }}>
-          <Card.Img variant="top" src="src\assets\blog.jpg" />
+          <Card.Img variant="top" src="../src/assets/blog.jpg" />
           <Card.Body>
             <Card.Title>Community Blogs</Card.Title>
             <Card.Text>
@@ -62,7 +62,7 @@ export default function Aboutus() {
           </Card.Body>
         </Card>
         <Card style={{ width: '24rem' }}>
-          <Card.Img variant="top" src="src\assets\artist_ecommerce.jpg" />
+          <Card.Img variant="top" src="../src/assets/artist_ecommerce.jpg" />
           <Card.Body>
             <Card.Title>Artists Section</Card.Title>
             <Card.Text>
@@ -72,44 +72,6 @@ export default function Aboutus() {
           </Card.Body>
         </Card>
       </div>
-
-
-
-
-
-
-
-
-
-
-        {/* <div className="aboutpara row">
-          <div className="col-md-6">
-            <div className="row " id="aboutpara1">
-              "The 'Garbage Guardians' offers a comprehensive solution to all
-              your garbage-related issues. Its special features include:
-            </div>
-            <div className="row " id="aboutpara2">
-              <ul>
-                <li>
-                  Lodging a complaint about your area by simply taking a photo
-                </li>
-                <li>
-                  Checking if your zone falls under the red zone category.
-                </li>
-                <li>Accessing the locations of nearby NGOs.</li>
-                <li>
-                  Interacting with others online to address your waste problems.
-                </li>
-                <li>Participating in quizzes to earn certifications.</li>
-                <li>
-                  Registering as an NGO and helping raise awareness about waste
-                  management in villages.
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className="col-md-5 offset-md-1 aboutleftimg"></div>
-        </div> */}
       </div>
     </div>
   );
