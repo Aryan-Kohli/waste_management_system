ec3788f59d14cc8ac49fa492a3b87ea7b626bcb4
Aryan-Kohli <106698463+Aryan-Kohli@users.noreply.github.com>
2023-09-10 13:02:54 +0530

    fixed image glitch

--- src/components/Aboutus.jsx
+++ src/components/Aboutus.jsx
@@ -1,7 +1,12 @@
 import "./Aboutus.css";
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import quizpic from "../assets/quiz.svg";
+import ngopic from "../assets/ngo.jpeg";
+import modulespic from "../assets/modules.jpg";
+import rewardpic from "../assets/reward.jpg";
+import blogpic from "../assets/blog.jpg";
+import artist_ecommercepic from "../assets/artist_ecommerce.jpg";
 
 export default function Aboutus() {
   return (
@@ -10,68 +15,60 @@ export default function Aboutus() {
         <div className="about_head">
           <h1 className="text-center">Features</h1>
         </div>
-      <div id = "Cards-stack">
-        <Card style={{ width: '24rem' }}>
-          <Card.Img variant="top" src="../src/assets/quiz.svg" />
-          <Card.Body>
-            <Card.Title>Interactive Quizzes</Card.Title>
-            <Card.Text>
-              This is Feature 1.
-            </Card.Text>
-            <Button className="btn-success">Know more.</Button>
-          </Card.Body>
-       </Card>
-       <Card style={{ width: '24rem' }}>
-          <Card.Img variant="top" src="../src/assets/ngo.jpeg" />
-          <Card.Body>
-            <Card.Title>Collaboration with Non Goverment Organizations.</Card.Title>
-            <Card.Text>
-              This is Feature 2.
-            </Card.Text>
-            <Button className="btn-success">Know more.</Button>
-          </Card.Body>
-        </Card>
-        <Card style={{ width: '24rem' }}>
-          <Card.Img variant="top" src="../src/assets/modules.jpg" />
-          <Card.Body>
-            <Card.Title>Informative Educational Modules and Resources</Card.Title>
-            <Card.Text>
-              This is Feature 3.
-            </Card.Text>
-            <Button className="btn-success">Know more.</Button>
-          </Card.Body>
-        </Card>
-        <Card style={{ width: '24rem' }}>
-          <Card.Img variant="top" src="../src/assets/reward.jpg" />
-          <Card.Body>
-            <Card.Title>Society of the month Competitions.</Card.Title>
-            <Card.Text>
-              This is Feature 4.
-            </Card.Text>
-            <Button className="btn-success">Know more.</Button>
-          </Card.Body>
-        </Card>
-        <Card style={{ width: '24rem' }}>
-          <Card.Img variant="top" src="../src/assets/blog.jpg" />
-          <Card.Body>
-            <Card.Title>Community Blogs</Card.Title>
-            <Card.Text>
-              This is Feature 5.
-            </Card.Text>
-            <Button className="btn-success">Know more.</Button>
-          </Card.Body>
-        </Card>
-        <Card style={{ width: '24rem' }}>
-          <Card.Img variant="top" src="../src/assets/artist_ecommerce.jpg" />
-          <Card.Body>
-            <Card.Title>Artists Section</Card.Title>
-            <Card.Text>
-              This is Feature 6.
-            </Card.Text>
-            <Button className="btn-success">Know more.</Button>
-          </Card.Body>
-        </Card>
-      </div>
+        <div id="Cards-stack">
+          <Card style={{ width: "24rem" }}>
+            <Card.Img variant="top" src={quizpic} />
+            <Card.Body>
+              <Card.Title>Interactive Quizzes</Card.Title>
+              <Card.Text>This is Feature 1.</Card.Text>
+              <Button className="btn-success">Know more.</Button>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: "24rem" }}>
+            <Card.Img variant="top" src={ngopic} />
+            <Card.Body>
+              <Card.Title>
+                Collaboration with Non Goverment Organizations.
+              </Card.Title>
+              <Card.Text>This is Feature 2.</Card.Text>
+              <Button className="btn-success">Know more.</Button>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: "24rem" }}>
+            <Card.Img variant="top" src={modulespic} />
+            <Card.Body>
+              <Card.Title>
+                Informative Educational Modules and Resources
+              </Card.Title>
+              <Card.Text>This is Feature 3.</Card.Text>
+              <Button className="btn-success">Know more.</Button>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: "24rem" }}>
+            <Card.Img variant="top" src={rewardpic} />
+            <Card.Body>
+              <Card.Title>Society of the month Competitions.</Card.Title>
+              <Card.Text>This is Feature 4.</Card.Text>
+              <Button className="btn-success">Know more.</Button>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: "24rem" }}>
+            <Card.Img variant="top" src={blogpic} />
+            <Card.Body>
+              <Card.Title>Community Blogs</Card.Title>
+              <Card.Text>This is Feature 5.</Card.Text>
+              <Button className="btn-success">Know more.</Button>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: "24rem" }}>
+            <Card.Img variant="top" src={artist_ecommercepic} />
+            <Card.Body>
+              <Card.Title>Artists Section</Card.Title>
+              <Card.Text>This is Feature 6.</Card.Text>
+              <Button className="btn-success">Know more.</Button>
+            </Card.Body>
+          </Card>
+        </div>
       </div>
     </div>
   );
