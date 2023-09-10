7789b524418d43195ad00812ce99c5c08e5fb766
Aryan-Kohli <106698463+Aryan-Kohli@users.noreply.github.com>
2023-09-10 13:55:05 +0530

    added carousel in hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,87 @@
 import "./Hero.css";
 
+import ngopic1 from "../assets/ngo1.jpeg";
+import ngopic2 from "../assets/ngo2.jpeg";
+import cleanpic1 from "../assets/clean.jpeg";
+import cleanpic2 from "../assets/clean2.jpeg";
+
 export default function Hero() {
   return (
-    <div id="hero-section">
-      <h1 id="hero">
-        An all in one
-        <br /> waste management system <br /> that lets you keep your area{" "}
-        <br /> CLEAN.
-      </h1>
+    <div id="hero">
+      <div
+        id="carouselExampleIndicators"
+        className="carousel carousel-dark slide"
+      >
+        <div className="carousel-indicators">
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="0"
+            className="active nexttimgbtn"
+            aria-current="true"
+            aria-label="Slide 1"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="1"
+            aria-label="Slide 2"
+            className="nexttimgbtn"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="2"
+            className="nexttimgbtn"
+            aria-label="Slide 3"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="3"
+            aria-label="Slide 4"
+            className="nexttimgbtn"
+          ></button>
+        </div>
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src={cleanpic1} className="d-block w-100" alt="..." />
+          </div>
+          <div className="carousel-item">
+            <img src={ngopic1} className="d-block w-100" alt="..." />
+          </div>
+          <div className="carousel-item">
+            <img src={cleanpic2} className="d-block w-100" alt="..." />
+          </div>
+          <div className="carousel-item">
+            <img src={ngopic2} className="d-block w-100" alt="..." />
+          </div>
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
     </div>
   );
 }
