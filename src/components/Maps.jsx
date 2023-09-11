a45ed82408c6f20ed5ebfb13219d3c4987f384fa
Aryan-Kohli <106698463+Aryan-Kohli@users.noreply.github.com>
2023-09-11 21:22:53 +0530

    updated bugs

--- src/components/Maps.jsx
+++ src/components/Maps.jsx
@@ -1,12 +1,12 @@
 import "./Maps.css";
 import { useState } from "react";
-import northeast from "../assets/mapspic/ne-Delhi.jpeg";
-import central from "../assets/mapspic/c-Delhi.jpeg";
-import east from "../assets/mapspic/e-Delhi.jpeg";
-import south from "../assets/mapspic/s-Delhi.jpeg";
-import southeast from "../assets/mapspic/se-Delhi.jpeg";
-import southwest from "../assets/mapspic/sw-Delhi.jpeg";
-import northwest from "../assets/mapspic/nw-Delhi.jpeg";
+import northeast from "../assets/mapspic/ne-delhi.jpeg";
+import central from "../assets/mapspic/c-delhi.jpeg";
+import east from "../assets/mapspic/e-delhi.jpeg";
+import south from "../assets/mapspic/s-delhi.jpeg";
+import southeast from "../assets/mapspic/se-delhi.jpeg";
+import southwest from "../assets/mapspic/sw-delhi.jpeg";
+import northwest from "../assets/mapspic/nw-delhi.jpeg";
 
 import Dropdown from "react-dropdown";
 import "react-dropdown/style.css";
